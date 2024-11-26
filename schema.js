4536ee5fa90026e9878eeb1b0b02f8e94d855ece
Neeraj Shukla <neerajgithub2024@gmail.com>
2024-11-26 13:36:50 +0530

    error_handling_2

---
+++ schema.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+// const defaultUrl = "https://images.unsplash.com/photo-1610155180135-015fe55f73cf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDMwfHx8ZW58MHx8fHx8";
+
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.object({
+            url: Joi.string().allow("", null),
+            filename: Joi.string().allow("", null)
+        }).optional()
+    }) .required()
+});
+
+// module.exports.reviewSchema = Joi.object({
+//     review: Joi.object({
+//         rating: Joi.number().require().min(1).max(5),
+//         comment: Joi.string().required(),
+//     }).required(),
+// })
