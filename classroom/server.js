7223e85fc715602d83472ea696d0c90eac003c8b
Neeraj Shukla <neerajgithub2024@gmail.com>
2024-12-16 12:55:26 +0530

    Cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,9 +2,38 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
 
+// Send Signed Cookies 
+
+app.use(cookieParser("secretcode"));
+
+app.get("/getsignedcookie", (req, res) => {
+    res.cookie("made-in", "India", { signed: true}); 
+    res.send("signed cookie send");
+});
+
+// Verify Signed Cookies
+
+app.get("/verify", (req, res) => {
+    console.log(req.signedCookies);
+    res.send("verified");
+});
+
+app.get("/getcookies", (req, res) => {
+    res.cookie("greet", "namaste");
+    res.cookie("origin", "India");
+    res.send("We sent you a cookie!");
+});  
+
+
+app.get("/greet", (req, res) => {
+    let { name = "anonymous" } = req.cookies;
+    res.send(`Hi, ${name}`);
+})
 
 app.get("/", (req, res) => {
+    console.dir(req.cookies);
     res.send("Hi, i am root!");
 });
 
